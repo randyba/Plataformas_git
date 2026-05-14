3d24e8a3d8b05ed7809914bfcbd36e0452dce44b
Randy Barrantes <100777494+randyba@users.noreply.github.com>
2026-05-14 14:42:31 -0600

    creacion de dos funciones que verifican que la base sea distinta de cero y que el exponente sea positivo

--- Laboratorio03/Ejercicio_1.c
+++ Laboratorio03/Ejercicio_1.c
@@ -11,9 +11,59 @@ int potencia(int base, int exponente){
     return resultado;
 }
 
+int verificar_exponente(){
+    
+    int variable_control = 0;
+    int exponente;
+    do
+    {
+        printf("ingrese el exponente: ");
+        scanf("%d", &exponente);
+        printf("\n");
+        if (exponente < 0)
+        {
+                printf("el exponente no puede ser negativo\n");
+        }
+        else
+        {
+            variable_control = 1;
+        }
+    } while (variable_control == 0);
+    
+    return exponente;
+}
+
+int verificar_base(){
+    
+    int variable_control = 0;
+    int base;
+    do
+    {
+        printf("ingrese la base: ");
+        printf("\n");
+        scanf("%d", &base);
+        if (base == 0)
+        {
+                printf("la base no puede ser cero\n");
+        }
+        else
+        {
+            variable_control = 1;
+        }
+    } while (variable_control == 0);
+    
+    return base;
+}
+
 int main(void){
-    printf("2^8 = %d\n",potencia(2 , 8 ));   
-    printf("3^4 = %d\n",potencia(3 , 4 ));
+
+    int exponente =verificar_exponente();
+    int base = verificar_base();
+    int resultado = potencia(base, exponente);
+    printf("%d elevado a %d es igual a %d\n", base, exponente, resultado);
+
+
+    
 
     return 0;
 }
