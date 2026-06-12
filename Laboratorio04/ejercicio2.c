06addbebefc00ad77a841fec99ef32bc76919e12
Randy Barrantes <100777494+randyba@users.noreply.github.com>
2026-06-11 21:57:06 -0600

    Escrita la funcion apply_threshold, veremos si funciona

--- Laboratorio04/ejercicio2.c
+++ Laboratorio04/ejercicio2.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 unsigned char *read_pgm (const char *filename, int *width, int *height, int *max_val){
 
 }
 
 void apply_threshold(unsigned char *pixels, int total, int threshold) {
+    unsigned char *actual = pixels; // este puntero va a los pixeles
+    unsigned char *fin = pixels + total; // va al final
 
+    while (actual < fin) { // preguntamos si es logico que no estemos en el final
+        if (*actual >= threshold) { // decimos que si el pixel es mayor al himbral, lo seteamos en blanco
+            *actual = 255;
+        } else {
+            *actual = 0; // si no es asi, lo seteamos en negro
+        }
+        actual++; // avanzamos en el recorrrido
+    }
+
+    // funcioncita mas dolor de cabezaaaa!!!!
 }
 
 unsigned char *make_negative(unsigned char *pixels, int total) {
