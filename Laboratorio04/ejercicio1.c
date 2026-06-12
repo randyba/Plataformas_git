5c14b21e1c6a0e784226a541ed6ed949e55e0d35
Randy Barrantes <100777494+randyba@users.noreply.github.com>
2026-06-11 21:18:47 -0600

    Arreglo para que lea la matriz de manera lineal omitiendo los saltos de linea para encontrar la cantidad de unos consecutivos mas grande

--- Laboratorio04/ejercicio1.c
+++ Laboratorio04/ejercicio1.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 
 void FindLargestLine(int **matrix, int size, int *result) {
-    int maxLine = 0;// este sera un contadorsillo que lleva el dato de unos mas largo
-    for (int i = 0; i < size; i++) {
-        int lineaactual = 0; //seteamos el contador de la linea actual a 0
-        for (int j = 0; j < size; j++) {
-            if (*(*(matrix + i) + j) == 1) { // en el if, vemos entrada por entrada si hay un uno
-                lineaactual++;// si se cumple, entonces sumamos uno al contador
-            } else {
-                if (lineaactual > maxLine) { // peguuntamos si el conador es mayor al maximo para actualizar
-                    maxLine = lineaactual; // defiino el nuevo valor que tendra el maximo en ese pinto
-                }
-                lineaactual = 0;// aqui seteo el contador para la siguiente linea.
+    int maxLine = 0; // este sera un contadorsillo que lleva el dato de unos mas largo
+    int lineaActual = 0; // contador de la secuencia actual de 1s
+    int total = size * size;
+    int *linear = *(matrix + 0); // puntero al primer elemento de la matriz
+
+    for (int i = 0; i < total; i++) {
+        if (*(linear + i) == 1) {
+            lineaActual++;
+        } else {
+            if (lineaActual > maxLine) {
+                maxLine = lineaActual;
             }
-        }
-        if (lineaactual > maxLine) {// pregnuro si el contador es mayor al maximo, para actualizar
-            maxLine = lineaactual;
+            lineaActual = 0;
         }
     }
-*result = maxLine; // pasamos el dato de la linea por debajo, por  el puntero *result
-return;
 
+    if (lineaActual > maxLine) {
+        maxLine = lineaActual;
+    }
+
+    *result = maxLine;
 }
 
 void allocateMatrix(int ***matrix, int size) {
-    *matrix = (int **)malloc(size * sizeof(int *));// se asigna o reserva la memoria para las entradas. esta parte es un poco compleja para mi.
-    for (int i = 0; i < size; i++) {
-        *(*matrix + i) = (int *)malloc(size * sizeof(int));// se reserva la memoria para cada fila, se ven como listas.
-    }
-    return;
+    int *data = (int *)malloc(size * size * sizeof(int));
+    *matrix = (int **)malloc(size * sizeof(int *));
 
+    for (int i = 0; i < size; i++) {
+        *(*matrix + i) = data + i * size;
+    }
 }
 
 void fillmatrix(int **matrix, int size) {
@@ -56,9 +57,7 @@ void printMatrix(int **matrix, int size) {
 }
 
 void freematrix(int **matrix, int size) {
-    for (int i = 0; i < size; i++) {
-        free(*(matrix + i));
-    }
+    free(*matrix); // libera el bloque contiguo de datos
     free(matrix);
 }
 
