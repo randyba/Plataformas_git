27e583c709e5d7c29b4ef68e5b555dcea96391a6
Randy Barrantes <100777494+randyba@users.noreply.github.com>
2026-06-11 21:44:29 -0600

    Copia del codigo estructura del ejercicio2.c

---
+++ Laboratorio04/ejercicio2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned char *read_pgm (const char *filename, int *width, int *height, int *max_val){
+
+}
+
+void apply_threshold(unsigned char *pixels, int total, int threshold) {
+
+}
+
+unsigned char *make_negative(unsigned char *pixels, int total) {
+
+}
+
+void write_pgm(const char *filename, unsigned char *pixels, int width, int height, int max_val) {
+
+}
+
+void print_stats(unsigned char *original, unsigned char *thresholded,int total) {
+
+}
+
+int main(void){
+    int width, height, max_val;
+    unsigned char *pixels = NULL;
+    unsigned char *negative = NULL;
+
+
+    return 0;
+
+}
