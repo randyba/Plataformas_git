90e7772aba3e412795215964a18a03855bfcf6b6
randyba <randybarrantes03@gmail.com>
2026-06-25 05:09:39 -0600

    creacion de los archivos: Funciones.h, Funciones.c y main.c

---
+++ Laboratorio05/src/Funciones.c
@@ -0,0 +1,7 @@
+/*
+funciones.c
+IE-0117 Laboratorio 05
+
+los camibios y manejo de la logica de las funciones se manejan en este archivo.
+*/
+#include "Funciones.h"
---
+++ Laboratorio05/src/Funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+/*
+funciones.h
+IE-0117 Laboratorio 05
+*/
+
+
+#endif // FUNCIONES_H
